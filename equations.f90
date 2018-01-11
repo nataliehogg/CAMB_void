1959dac362dd5e0529c193fa76e85a9816fd82a0
Matteo Martinelli <martinelli@potter.lorentz.leidenuniv.nl>
2018-01-11 18:52:23 +0100

    flagged modifications to do in equations.f90. equations_ppf has no DE clustering by definition

--- equations.f90
+++ equations.f90
@@ -1228,12 +1228,12 @@
     !  Compute expansion rate from: grho 8*pi*rho*a**2
 
     grhob_t=grhob/a
-    grhoc_t=grhoc/a
+    grhoc_t=grhoc/a !VOID: change a dependence with Eqs. (7-8), (20-21)
     grhor_t=grhornomass/a2
     grhog_t=grhog/a2
-    grhov_t=grhov*a**(-1-3*w_lam)
+    grhov_t=grhov*a**(-1-3*w_lam) !VOID: change a dependence with Eqs. (7-8), (20-21)
     grho=grhob_t+grhoc_t+grhor_t+grhog_t+grhov_t
-    gpres=(grhog_t+grhor_t)/3+grhov_t*w_lam
+    gpres=(grhog_t+grhor_t)/3+grhov_t*w_lam !VOID: change a dependence with Eqs. (7-8), (20-21)
 
     !  8*pi*a*a*SUM[rho_i*clx_i] add radiation later
     dgrho=grhob_t*clxb+grhoc_t*clxc
@@ -1249,7 +1249,7 @@
         call MassiveNuVarsOut(EV,y,yprime,a,grho,gpres,dgrho,dgq,dgpi, dgpi_diff,pidot_sum)
     end if
 
-    if (w_lam /= -1 .and. w_Perturb) then
+    if (w_lam /= -1 .and. w_Perturb) then  !VOID: check changes to be done here
         clxq=y(EV%w_ix)
         vq=y(EV%w_ix+1)
         dgrho=dgrho + clxq*grhov_t
@@ -1967,10 +1967,10 @@
     !  Compute expansion rate from: grho 8*pi*rho*a**2
 
     grhob_t=grhob/a
-    grhoc_t=grhoc/a
+    grhoc_t=grhoc/a !VOID: change a dependence with Eqs. (7-8), (20-21)
     grhor_t=grhornomass/a2
     grhog_t=grhog/a2
-    if (w_lam==-1._dl) then
+    if (w_lam==-1._dl) then !VOID: change a dependence with Eqs. (7-8), (20-21)
         grhov_t=grhov*a2
     else
         grhov_t=grhov*a**(-1-3*w_lam)
@@ -2008,7 +2008,7 @@
 
     dgrho = dgrho_matter
 
-    if (w_lam /= -1 .and. w_Perturb) then
+    if (w_lam /= -1 .and. w_Perturb) then !VOID: check changes to do here
         clxq=ay(EV%w_ix)
         vq=ay(EV%w_ix+1)
         dgrho=dgrho + clxq*grhov_t
@@ -2073,7 +2073,7 @@
         ayprime(2)=0.5_dl*dgq + CP%curv*z
     end if
 
-    if (w_lam /= -1 .and. w_Perturb) then
+    if (w_lam /= -1 .and. w_Perturb) then !VOID: change for new DE clustering
         ayprime(EV%w_ix)= -3*adotoa*(cs2_lam-w_lam)*(clxq+3*adotoa*(1+w_lam)*vq/k) &
             -(1+w_lam)*k*vq -(1+w_lam)*k*z
 
@@ -2102,7 +2102,7 @@
         EV%OutputTransfer(Transfer_vel_baryon_cdm) = vb
     end if
 
-    !  CDM equation of motion
+    !  CDM equation of motion !VOID: change for new DM clustering
     clxcdot=-k*z
     ayprime(3)=clxcdot
 
@@ -2523,10 +2523,10 @@
     ! Compute expansion rate from: grho=8*pi*rho*a**2
     ! Also calculate gpres: 8*pi*p*a**2
     grhob_t=grhob/a
-    grhoc_t=grhoc/a
+    grhoc_t=grhoc/a !VOID: change a dependence with Eqs. (7-8), (20-21)
     grhor_t=grhornomass/a2
     grhog_t=grhog/a2
-    if (w_lam==-1._dl) then
+    if (w_lam==-1._dl) then!VOID: change a dependence with Eqs. (7-8), (20-21)
         grhov_t=grhov*a2
     else
         grhov_t=grhov*a**(-1-3*w_lam)
